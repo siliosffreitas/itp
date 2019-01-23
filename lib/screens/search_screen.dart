ada0ecea557ad328ef7198d2a075c8710780c953
Silio <siliosilvestre@infoway-pi.com.br>
2019-01-23 10:30:58 -0300

    configuracao do firebase no ios

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -7,6 +8,27 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('baby').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Center(child: CircularProgressIndicator(),);
+//        _buildList(context, snapshot.data.documents)
+        print(snapshot.data.documents);
+        snapshot.data.documents.map((doc) {
+          print(doc);
+        });
+        print('teste');
+        return Text("teste");
+      },
+    );
+  }
+
+//  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+//    final record = Record.fromSnapshot(data);
+//  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,15 +37,17 @@ class _SearchScreenState extends State<SearchScreen> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: (){},
+            onPressed: () {},
             tooltip: "Pesquisar",
           )
         ],
       ),
-     body: FutureBuilder(
-         builder: null
-     ),
+
+      body: _buildBody(context),
+
+//     body: FutureBuilder(
+//         builder: null
+//     ),
     );
   }
 }
-
