eda6578eda67e884b88a378d59ada58b9c3cd758
Silio <siliosilvestre@infoway-pi.com.br>
2019-01-29 09:18:09 -0300

    configuracao do maps no android

--- lib/screens/tabs/map_tab.dart
+++ lib/screens/tabs/map_tab.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapTab extends StatelessWidget {
+class MapTab extends StatefulWidget {
+  @override
+  _MapTabState createState() => _MapTabState();
+}
+
+class _MapTabState extends State<MapTab> {
+  GoogleMapController mapController;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(-5.082618, -42.790596),
+          zoom: 11,
+        ),
+      ),
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
   }
 }
