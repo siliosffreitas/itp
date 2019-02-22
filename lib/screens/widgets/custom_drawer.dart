90bd9c02b537768e0fcd59eee9b1c27172b31969
Silio <siliosilvestre@infoway-pi.com.br>
2019-02-22 11:41:16 -0300

    remocao de código extra

--- lib/screens/widgets/custom_drawer.dart
+++ lib/screens/widgets/custom_drawer.dart
@@ -34,75 +34,28 @@ class CustomDrawer extends StatelessWidget {
                     "http://www.sclance.com/pngs/profile-icon-png/profile_icon_png_1113474.png"),
               ),
             ),
-
-//              GestureDetector(
-//                onTap: () => print('clicou na imagem de perfil'),
-//                child: CircleAvatar(
-//                  backgroundImage: NetworkImage(
-//                      "https://www.machados.pe.gov.br/portal/img/secretarias/_img_22208.png"),
-//                ),
-//              ),
-//
-//              GestureDetector(
-//                onTap: () => print('clicou na imagem de perfil'),
-//                child: CircleAvatar(
-//                  backgroundImage: NetworkImage(
-//                      "https://www.shareicon.net/data/128x128/2016/08/18/810252_user_512x512.png"),
-//                ),
-//              ),
-//
-//              GestureDetector(
-//                onTap: () => print('clicou na imagem de perfil'),
-//                child: CircleAvatar(
-//                  backgroundImage: NetworkImage(
-//                      "https://img2.gratispng.com/20180611/hq/kisspng-computer-icons-entrepreneurship-businessperson-cli-5b1eb37bb81147.776451701528738683754.jpg"),
-//                ),
-//              )
           ],
           onDetailsPressed: () => print("clique nos detalhes"),
         ),
-
         Expanded(
           child: Container(
-//    color: Colors.red,
               child: ListView(
             children: <Widget>[
               DrawerTile(Icons.home, "Início", _pageController, 0),
-                        DrawerTile(Icons.place, "Informar veículo", _pageController, 6),
-          DrawerTile(Icons.message, "Fale com a gente", _pageController, 2),
-          DrawerTile(Icons.share, "Compartilhar", _pageController, 3),
-          DrawerTile(Icons.home, "Tutorial", _pageController, 4),
-          DrawerTile(Icons.info, "Sobre", _pageController, 5),
-
+              DrawerTile(Icons.place, "Informar veículo", _pageController, 6),
+              DrawerTile(Icons.message, "Fale com a gente", _pageController, 2),
+              DrawerTile(Icons.share, "Compartilhar", _pageController, 3),
+              DrawerTile(Icons.home, "Tutorial", _pageController, 4),
+              DrawerTile(Icons.info, "Sobre", _pageController, 5),
             ],
           )),
         ),
-        Divider(height: 1,),
-        DrawerTile(Icons.settings, "Configurações", _pageController, 1),
-
-//            ListView(
-//              children: <Widget>[
-//
-
-////          DrawerTile(Icons.place, "Informar veículo", _pageController, 1),
-////          DrawerTile(Icons.message, "Fale com a gente", _pageController, 2),
-////          DrawerTile(Icons.share, "Compartilhar", _pageController, 3),
-////          DrawerTile(Icons.home, "Tutorial", _pageController, 4),
-////          DrawerTile(Icons.info, "Sobre", _pageController, 5),
-//                DrawerTile(Icons.settings, "Configurações", _pageController, 1),
-//              ],
-//            )
+        Material(
+          elevation: 10,
+          child:
+              DrawerTile(Icons.settings, "Configurações", _pageController, 1),
+        )
       ],
-    )
-
-//        SingleChildScrollView(
-//      child: Column(
-//        children: <Widget>[
-//
-//
-//        ],
-//      ),
-//    )
-        );
+    ));
   }
 }
